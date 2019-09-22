b215f91f253ad0b6b3eebeaf525201f13436ded4
Victor-733 <victorwahid@gmail.com>
2019-09-22 14:55:27 +0000

    add comments

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # VMF_Verkefni2
+
+## tutorial sem ég studdist við:
+https://www.youtube.com/watch?v=kju9OgYrUmU&list=PL2935W76vRNHFpPUuqmLoGCzwx_8eq5yK&index=3
--- triangle/triangle.js
+++ triangle/triangle.js
@@ -2,20 +2,58 @@
 // start here
 //
 function main() {
-  const canvas = document.querySelector("#glCanvas");
-  // Initialize the GL context
-  const gl = canvas.getContext("webgl");
+  // kallar á canvas og webgl
+  const canvas = document.querySelector('canvas');
+  const gl = canvas.getContext('webgl');
 
-  // Only continue if WebGL is available and working
-  if (gl === null) {
-    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-    return;
+  // ef að browserinn styður ekki webgl byrtist error
+  if (!gl) {
+      throw new Error('WebGL not supported');
   }
 
-  // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  // hnitin fyrir þríhyrninginn
+  const vertexData = [
+      0, 1, 0,
+      1, -1, 0,
+      -1, -1, 0,
+  ];
+
+  // búa til buffer sem að við munum loada vertex í
+  const buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+  // búa til shaders sem að birta hnitin úr bufferinu sem við vorum að gera
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShader, `
+  attribute vec3 position;
+  void main() {
+      gl_Position = vec4(position, 1);
+  }
+  `);
+  gl.compileShader(vertexShader);
+
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShader, `
+  void main() {
+      gl_FragColor = vec4(1, 0, 0, 1);
+  }
+  `);
+  gl.compileShader(fragmentShader);
+
+  // búa til program og loada shaderunum sem við vorum að búa til í programið
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+
+  // staðsetningin á attribute sem að hann á að loada
+  const positionLocation = gl.getAttribLocation(program, `position`);
+  gl.enableVertexAttribArray(positionLocation);
+  gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+
+  gl.useProgram(program);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 
 window.onload = main;
