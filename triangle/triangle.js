2fe88e960c9d0d618369329495c705dadfc3b427
Victor-733 <victorwahid@gmail.com>
2019-09-22 15:19:53 +0000

    add colors to triangle

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -18,16 +18,33 @@ function main() {
       -1, -1, 0,
   ];
 
+  // RGB gildi fyrir liti
+  const colorData = [
+    1, 0, 0, // litur á horni/vertex 1
+    0, 1, 0, // litur á horni/vertex 2
+    0, 0, 1, // litur á horni/vertex 3
+  ];
+
   // búa til buffer sem að við munum loada vertex í
-  const buffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  const positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexData), gl.STATIC_DRAW);
+
+  // búa til annað buffer fyrir liti
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorData), gl.STATIC_DRAW);
 
   // búa til shaders sem að birta hnitin úr bufferinu sem við vorum að gera
   const vertexShader = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vertexShader, `
+  precision mediump float;
   attribute vec3 position;
+  attribute vec3 color;
+  varying vec3 vColor;
+
   void main() {
+      vColor = color;
       gl_Position = vec4(position, 1);
   }
   `);
@@ -35,8 +52,12 @@ function main() {
 
   const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
   gl.shaderSource(fragmentShader, `
+  precision mediump float;
+
+  varying vec3 vColor;
+
   void main() {
-      gl_FragColor = vec4(1, 0, 0, 1);
+      gl_FragColor = vec4(vColor, 1);
   }
   `);
   gl.compileShader(fragmentShader);
@@ -50,8 +71,15 @@ function main() {
   // staðsetningin á attribute sem að hann á að loada
   const positionLocation = gl.getAttribLocation(program, `position`);
   gl.enableVertexAttribArray(positionLocation);
-  gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer); // nota positionBuffer hér með því að binda það
+  gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0); // lesa 3 hnit í einu
 
+  const colorLocation = gl.getAttribLocation(program, `color`);
+  gl.enableVertexAttribArray(colorLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer); // nota colorBuffer hér
+  gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
+
+  // segja forritinu hvaða program það á að teikna
   gl.useProgram(program);
   gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
