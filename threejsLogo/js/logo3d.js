9b194bfe19523c3eb0be738252e1188fd2afbd09
Victor-733 <victorwahid@gmail.com>
2019-09-29 22:12:51 +0000

    do you wanna build a snowman?

--- threejsLogo/js/logo3d.js
+++ threejsLogo/js/logo3d.js
@@ -1,11 +1,11 @@
-var scene = new THREE.Scene(); // búa til scene
+let scene = new THREE.Scene(); // búa til scene
 
 // búa til camera sem er með (FOV, svæði, near & far plane)
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
 // búa til renderer sem að renderar scene-ið okkar
-var renderer = new THREE.WebGLRenderer( {antialias: true}); // antialias gerir útlínurnar ekki eins grófar
+let renderer = new THREE.WebGLRenderer( {antialias: true}); // antialias gerir útlínurnar ekki eins grófar
 renderer.setClearColor("#e5e5e5"); // bakgrunnslitur
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -15,27 +15,39 @@ window.addEventListener('resize', () => { // event listener sem að rezise-ar sc
     renderer.setSize(window.innerWidth, window.innerHeight);
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-})
+});
 
 renderer.render(scene, camera);
 
-var geometry = new THREE.BoxGeometry(1, 1, 1); // búa til shape
-var material = new THREE.MeshLambertMaterial({color: 0xFFBACA}); // búa til material (MeshLambertMaterial)
-var mesh = new THREE.Mesh(geometry, material); // blanda forminu og material Í mesh
+let group = new THREE.Object3D();
+let geometry = new THREE.BoxGeometry(1, 1, 1); // búa til shape
+let geometry2 = new THREE.BoxGeometry(0.7, 0.7, 0.7);
+let geometry3 = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+let material = new THREE.MeshLambertMaterial({color: 0xFFBACA}); // búa til material (MeshLambertMaterial)
+let mesh = new THREE.Mesh(geometry, material); // blanda forminu og material Í mesh
+let mesh2 = new THREE.Mesh(geometry2, material);
+let mesh3 = new THREE.Mesh(geometry3, material);
 
-scene.add(mesh);
+group.add(mesh);
+group.add(mesh2);
+group.add(mesh3); // adda kössum í grúppuna
 
-var light = new THREE.PointLight(0xFFFFFF, 1, 500); // búa til ljós sem hefur (lit, intensity, distance)
+scene.add(group); // adda grúpunni í scene-ið
+
+mesh2.position.y = 0.8;
+mesh3.position.y = 1.4;
+
+let light = new THREE.PointLight(0xFFFFFF, 1, 500); // búa til ljós sem hefur (lit, intensity, distance)
 light.position.set(10, 0, 25); // staðsetning á ljósinu
 scene.add(light);
 
 let directionX = 0.01;
 let directionY = 0.01;
 
-var render = function() { // teiknar scene-ið alltaf aftur um leið og glugginn er breyttur eða refreshaður
+let render = function() { // teiknar scene-ið alltaf aftur um leið og glugginn er breyttur eða refreshaður
     requestAnimationFrame(render);
-    mesh.rotation.x += directionX; // kubburinn snýst
-    mesh.rotation.y += directionY;
+    group.rotation.x += directionX; // kubburinn snýst
+    group.rotation.y += directionY;
     renderer.render(scene, camera);
 }
 
