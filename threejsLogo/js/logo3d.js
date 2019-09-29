c43ef145fcf70dc1428580e5759b3d38cdcaa519
Victor-733 <victorwahid@gmail.com>
2019-09-29 15:28:22 +0000

    make basic shape to start logo off

--- threejsLogo/js/logo3d.js
+++ threejsLogo/js/logo3d.js
@@ -1,23 +1,41 @@
 var scene = new THREE.Scene(); // búa til scene
-var camera = new // búa til camera (aspect ratio, stærð á glugga, near distance, far distance)
-    THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-var renderer = new THREE.WebGLRenderer(); // renderer sem að renderar scene
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-// basic grænn kubbur
-var geometry = new THREE.BoxGeometry( 4, 2, 1 ); // stærðir á x y z ásum fyrir kassann
-var material = new THREE.MeshBasicMaterial( { color: 0xfffffff } ); // material sem hann er úr
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube ); // bæta við scene-ið
-
+// búa til camera sem er með (FOV, svæði, near & far plane)
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
 
-function animate() { // animate fall sem að updater á 60fps
-    requestAnimationFrame( animate );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-	renderer.render( scene, camera );
+// búa til renderer sem að renderar scene-ið okkar
+var renderer = new THREE.WebGLRenderer( {antialias: true}); // antialias gerir útlínurnar ekki eins grófar
+renderer.setClearColor("#e5e5e5"); // bakgrunnslitur
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+window.addEventListener('resize', () => { // event listener sem að rezise-ar scene-ið ef að glugginn breytist
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+})
+
+renderer.render(scene, camera);
+
+var geometry = new THREE.BoxGeometry(1, 1, 1); // búa til shape
+var material = new THREE.MeshLambertMaterial({color: 0xFFBACA}); // búa til material (MeshLambertMaterial)
+var mesh = new THREE.Mesh(geometry, material); // blanda forminu og material Í mesh
+
+mesh.position.x = 2;
+
+scene.add(mesh);
+
+var light = new THREE.PointLight(0xFFFFFF, 1, 500); // búa til ljós sem hefur (lit, intensity, distance)
+light.position.set(10, 0, 25); // staðsetning á ljósinu
+scene.add(light);
+
+var render = function() { // teiknar scene-ið alltaf aftur um leið og glugginn er breyttur eða refreshaður
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
 }
-animate();
+
+render();
+
+renderer.render(scene, camera);
